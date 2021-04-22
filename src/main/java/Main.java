856d9f100a78ca344056e5fb873f2c0391b6af78
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-04-22 16:35:40 +0300

    fe progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,16 +14,16 @@ public class Main {
     public static int validDataCount;
 
     public static void main(String[] args) throws Exception {
-        /*ImportData importer = new ImportData(trainPath);
+        ImportData importer = new ImportData(trainPath);
         importer.getData();
         List<PieceOfNews> trainData = importer.getArticles();
         System.out.println("TRAIN DATA : FOUND " + trainData.size() + " ARTICLES");
         trainDataCount =(int)(0.8* trainData.size());
-        /*Statistics statistics = new Statistics(trainData);
+        Statistics statistics = new Statistics(trainData);
         statistics.createBarChart();
-        Utils.serializeXML(trainData);*/
+        Utils.serializeXML(trainData);
 
-        /*importer = new ImportData(testPath);
+        importer = new ImportData(testPath);
         importer.getData();
         List<PieceOfNews> testData = importer.getArticles();
         System.out.println("TEST DATA : FOUND " + testData.size() + " ARTICLES");
@@ -31,8 +31,6 @@ public class Main {
         validDataCount =(int)(0.2 * testDataCount);
 
         Utils.generateReport();
-        */
-
         Server.run();
     }
 
